90b4772da148d7df84a6643cc5e975dec18e8225
joyersol <joyer1339@gmail.com>
2021-03-09 15:39:24 +0700

    Update background.js

--- background.js
+++ background.js
@@ -4,15 +4,5 @@ chrome.tabs.onUpdated.addListener(function(){
         file: 'inject.js'
     })
 })
-chrome.commands.onCommand.addListener(function (command) {
-    switch (command) {
-        case "play":
-            break;
-        case "next":
-            document. 
-            break;
-        default: 
-            alert("Recieved bad command")
-    }
 })
 
